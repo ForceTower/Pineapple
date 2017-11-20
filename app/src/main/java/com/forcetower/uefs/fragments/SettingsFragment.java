31763d3466522e2acee74e27076d081b02e0ba8e
João Paulo <joaopaulo761@gmail.com>
2017-11-20 00:01:36 -0300

    Logoff is working now

--- app/src/main/java/com/forcetower/uefs/fragments/SettingsFragment.java
+++ app/src/main/java/com/forcetower/uefs/fragments/SettingsFragment.java
@@ -1,14 +1,22 @@
 package com.forcetower.uefs.fragments;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.IntentCompat;
+import android.util.Log;
 
+import com.forcetower.uefs.Constants;
 import com.forcetower.uefs.R;
+import com.forcetower.uefs.activity.LoginActivity;
+import com.forcetower.uefs.sagres_sdk.domain.SagresAccess;
+import com.forcetower.uefs.sagres_sdk.domain.SagresProfile;
 
-public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener, Preference.OnPreferenceClickListener {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +27,16 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
     public void onResume() {
         super.onResume();
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        Preference logoff = findPreference("logoff_key");
+        if(logoff != null) logoff.setOnPreferenceClickListener(this);
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        Preference logoff = findPreference("logoff_key");
+        if(logoff != null) logoff.setOnPreferenceClickListener(this);
+
         getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 
@@ -47,5 +60,18 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
             }
         }
 
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        SagresProfile.setCurrentProfile(null);
+        SagresAccess.setCurrentAccess(null);
+
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        ActivityCompat.finishAffinity(getActivity());
+        return true;
     }
 }
