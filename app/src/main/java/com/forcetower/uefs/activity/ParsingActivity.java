c2a839c0e0a99b6a201451cbf04adc79918406d6
João Paulo <joaopaulo761@gmail.com>
2017-12-04 23:14:59 -0300

    Fixed bug where the start up of the app were to fast and checks would go right

--- app/src/main/java/com/forcetower/uefs/activity/ParsingActivity.java
+++ app/src/main/java/com/forcetower/uefs/activity/ParsingActivity.java
@@ -3,6 +3,7 @@ package com.forcetower.uefs.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.widget.TextView;
 
@@ -10,10 +11,12 @@ import com.forcetower.uefs.R;
 import com.forcetower.uefs.activity.base.UEFSBaseActivity;
 import com.forcetower.uefs.helpers.Utils;
 import com.forcetower.uefs.sagres_sdk.SagresPortalSDK;
+import com.forcetower.uefs.sagres_sdk.domain.SagresProfile;
 
 public class ParsingActivity extends UEFSBaseActivity {
     private TextView tv_information;
     private TextView tv_user_name;
+    private Handler handler;
 
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, ParsingActivity.class);
@@ -28,6 +31,8 @@ public class ParsingActivity extends UEFSBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parsing);
+        handler = new Handler(getMainLooper());
+
         tv_information = findViewById(R.id.tv_information);
         tv_information.setText(R.string.processing_info);
         tv_user_name = findViewById(R.id.tv_user_name);
@@ -37,13 +42,22 @@ public class ParsingActivity extends UEFSBaseActivity {
         SagresPortalSDK.addLoginListener(new SagresPortalSDK.LoginListener() {
             @Override
             public void alert(int code, String string) {
-                if (code == 10) {
+                if (code == 10 && !isFinishing()) {
                     finish();
                     return;
                 }
                 onReceiveUpdate(code, string);
             }
         });
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (SagresProfile.getCurrentProfile() != null) {
+                    finish();
+                }
+            }
+        }, 1500);
     }
 
     private void onReceiveUpdate(final int code, final String string) {
