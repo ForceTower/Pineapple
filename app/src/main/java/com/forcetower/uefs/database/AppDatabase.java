648a967bb9e8bd450bfc3457a06013922d27e96d
João Paulo <joaopaulo761@gmail.com>
2017-12-29 14:26:08 -0300

    Grade Info

--- app/src/main/java/com/forcetower/uefs/database/AppDatabase.java
+++ app/src/main/java/com/forcetower/uefs/database/AppDatabase.java
@@ -7,12 +7,14 @@ import com.forcetower.uefs.database.dao.AAccessDao;
 import com.forcetower.uefs.database.dao.ACalendarItemDao;
 import com.forcetower.uefs.database.dao.ADisciplineDao;
 import com.forcetower.uefs.database.dao.AGradeDao;
+import com.forcetower.uefs.database.dao.AGradeInfoDao;
 import com.forcetower.uefs.database.dao.AGradeSectionDao;
 import com.forcetower.uefs.database.dao.ASemesterDao;
 import com.forcetower.uefs.database.entities.AAccess;
 import com.forcetower.uefs.database.entities.ACalendarItem;
 import com.forcetower.uefs.database.entities.ADiscipline;
 import com.forcetower.uefs.database.entities.AGrade;
+import com.forcetower.uefs.database.entities.AGradeInfo;
 import com.forcetower.uefs.database.entities.AGradeSection;
 import com.forcetower.uefs.database.entities.ASemester;
 
@@ -25,7 +27,8 @@ import com.forcetower.uefs.database.entities.ASemester;
         ASemester.class,
         ADiscipline.class,
         AGrade.class,
-        AGradeSection.class},
+        AGradeSection.class,
+        AGradeInfo.class},
         version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract AAccessDao aAccessDao();
@@ -34,4 +37,5 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract ADisciplineDao aDisciplineDao();
     public abstract AGradeDao aGradeDao();
     public abstract AGradeSectionDao aGradeSectionDao();
+    public abstract AGradeInfoDao aGradeInfoDao();
 }
