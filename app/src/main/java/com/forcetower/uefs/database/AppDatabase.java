2f5f7298cd686fb641df7bd7dbc97471620e94d9
João Paulo <joaopaulo761@gmail.com>
2017-12-29 14:35:26 -0300

    Scrap remembers me of Orkut times... may it rest in peace

--- app/src/main/java/com/forcetower/uefs/database/AppDatabase.java
+++ app/src/main/java/com/forcetower/uefs/database/AppDatabase.java
@@ -9,6 +9,7 @@ import com.forcetower.uefs.database.dao.ADisciplineDao;
 import com.forcetower.uefs.database.dao.AGradeDao;
 import com.forcetower.uefs.database.dao.AGradeInfoDao;
 import com.forcetower.uefs.database.dao.AGradeSectionDao;
+import com.forcetower.uefs.database.dao.AScrapDao;
 import com.forcetower.uefs.database.dao.ASemesterDao;
 import com.forcetower.uefs.database.entities.AAccess;
 import com.forcetower.uefs.database.entities.ACalendarItem;
@@ -16,6 +17,7 @@ import com.forcetower.uefs.database.entities.ADiscipline;
 import com.forcetower.uefs.database.entities.AGrade;
 import com.forcetower.uefs.database.entities.AGradeInfo;
 import com.forcetower.uefs.database.entities.AGradeSection;
+import com.forcetower.uefs.database.entities.AScrap;
 import com.forcetower.uefs.database.entities.ASemester;
 
 /**
@@ -28,7 +30,8 @@ import com.forcetower.uefs.database.entities.ASemester;
         ADiscipline.class,
         AGrade.class,
         AGradeSection.class,
-        AGradeInfo.class},
+        AGradeInfo.class,
+        AScrap.class},
         version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract AAccessDao aAccessDao();
@@ -38,4 +41,5 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract AGradeDao aGradeDao();
     public abstract AGradeSectionDao aGradeSectionDao();
     public abstract AGradeInfoDao aGradeInfoDao();
+    public abstract AScrapDao aScrapDao();
 }
---
+++ app/src/main/java/com/forcetower/uefs/database/dao/AScrapDao.java
@@ -0,0 +1,31 @@
+package com.forcetower.uefs.database.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.forcetower.uefs.database.entities.AScrap;
+
+import java.util.List;
+
+/**
+ * Created by João Paulo on 29/12/2017.
+ */
+@Dao
+public interface AScrapDao {
+    @Query("SELECT * FROM AScrap")
+    List<AScrap> getAllScraps();
+
+    @Query("SELECT * FROM AScrap WHERE message LIKE :message AND sender LIKE :sender")
+    List<AScrap> getScraps(String message, String sender);
+
+    @Insert
+    void insertScraps(AScrap... scraps);
+
+    @Delete
+    void deleteScrap(AScrap scrap);
+
+    @Query("DELETE FROM AScrap")
+    void deleteAllScraps();
+}
