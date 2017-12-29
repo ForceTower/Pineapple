549c1936a85d3db071ebc69a7988fac89fb814a6
João Paulo <joaopaulo761@gmail.com>
2017-12-29 12:53:57 -0300

    Semester

--- app/src/main/java/com/forcetower/uefs/database/AppDatabase.java
+++ app/src/main/java/com/forcetower/uefs/database/AppDatabase.java
@@ -5,17 +5,21 @@ import android.arch.persistence.room.RoomDatabase;
 
 import com.forcetower.uefs.database.dao.AAccessDao;
 import com.forcetower.uefs.database.dao.ACalendarItemDao;
+import com.forcetower.uefs.database.dao.ASemesterDao;
 import com.forcetower.uefs.database.entities.AAccess;
 import com.forcetower.uefs.database.entities.ACalendarItem;
+import com.forcetower.uefs.database.entities.ASemester;
 
 /**
  * Created by João Paulo on 29/12/2017.
  */
 @Database(entities = {
         AAccess.class,
-        ACalendarItem.class},
+        ACalendarItem.class,
+        ASemester.class},
         version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract AAccessDao aAccessDao();
     public abstract ACalendarItemDao aCalendarItemDao();
+    public abstract ASemesterDao aSemesterDao();
 }
---
+++ app/src/main/java/com/forcetower/uefs/database/entities/ASemester.java
@@ -0,0 +1,46 @@
+package com.forcetower.uefs.database.entities;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by João Paulo on 29/12/2017.
+ */
+@Entity
+public class ASemester {
+    @PrimaryKey(autoGenerate = true)
+    private int uid;
+    @ColumnInfo(name = "uefs_id")
+    private String uefsId;
+    private String name;
+
+    public ASemester(String uefsId, String name) {
+        this.uefsId = uefsId;
+        this.name = name;
+    }
+
+    public String getUefsId() {
+        return uefsId;
+    }
+
+    public void setUefsId(String uefsId) {
+        this.uefsId = uefsId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+}
