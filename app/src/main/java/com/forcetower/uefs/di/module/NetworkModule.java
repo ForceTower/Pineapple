e213815f3f58975576b4d78b9844f3de33677254
João Paulo <joaopaulo761@gmail.com>
2018-03-26 21:12:54 -0300

    45 seconds to fail

--- app/src/main/java/com/forcetower/uefs/di/module/NetworkModule.java
+++ app/src/main/java/com/forcetower/uefs/di/module/NetworkModule.java
@@ -41,9 +41,9 @@ public class NetworkModule {
         return new OkHttpClient.Builder()
                 .followRedirects(true)
                 .cookieJar(cookieJar)
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .writeTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(45, TimeUnit.SECONDS)
+                .readTimeout(45, TimeUnit.SECONDS)
+                .writeTimeout(45, TimeUnit.SECONDS)
                 .build();
     }
 
