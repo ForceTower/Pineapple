6b44052e8aec678bac7e48ad2ca37b718d6c27d2
João Paulo <joaopaulo761@gmail.com>
2018-03-29 23:26:53 -0300

    Show locations

--- app/src/main/java/com/forcetower/uefs/view/connected/fragments/NewScheduleFragment.java
+++ app/src/main/java/com/forcetower/uefs/view/connected/fragments/NewScheduleFragment.java
@@ -12,12 +12,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 import com.forcetower.uefs.R;
 import com.forcetower.uefs.db.entity.DisciplineClassLocation;
 import com.forcetower.uefs.di.Injectable;
 import com.forcetower.uefs.util.AnimUtils;
 import com.forcetower.uefs.view.connected.adapters.NewScheduleAdapter;
+import com.forcetower.uefs.view.connected.adapters.ScheduleAdapter;
 import com.forcetower.uefs.vm.ScheduleViewModel;
 
 import java.util.ArrayList;
@@ -36,12 +38,15 @@ public class NewScheduleFragment extends Fragment implements Injectable {
     ViewGroup vgNoSchedule;
     @BindView(R.id.recycler_view)
     RecyclerView rvSchedule;
-    @BindView(R.id.cv_schedule)
-    CardView cvSchedule;
+    @BindView(R.id.rv_schedule_subtitle)
+    RecyclerView rvScheduleSubtitle;
+    @BindView(R.id.sv_schedule)
+    ScrollView svSchedule;
 
     @Inject
     ViewModelProvider.Factory viewModelFactory;
     private NewScheduleAdapter scheduleAdapter;
+    private ScheduleAdapter subtitleAdapter;
 
     @Nullable
     @Override
@@ -49,6 +54,7 @@ public class NewScheduleFragment extends Fragment implements Injectable {
         View view = inflater.inflate(R.layout.fragment_schedule_new, container, false);
         ButterKnife.bind(this, view);
         setupRecycler();
+        setupSubtitles();
         return view;
     }
 
@@ -63,17 +69,26 @@ public class NewScheduleFragment extends Fragment implements Injectable {
         scheduleAdapter = new NewScheduleAdapter(getContext(), new ArrayList<>());
         rvSchedule.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
         rvSchedule.setAdapter(scheduleAdapter);
+        rvSchedule.setNestedScrollingEnabled(false);
+    }
+
+    private void setupSubtitles() {
+        subtitleAdapter = new ScheduleAdapter(getContext(), new ArrayList<>());
+        rvScheduleSubtitle.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvScheduleSubtitle.setAdapter(subtitleAdapter);
+        rvScheduleSubtitle.setNestedScrollingEnabled(false);
     }
 
     private void onReceiveLocations(List<DisciplineClassLocation> locations) {
         if (locations == null || locations.isEmpty()) {
-            AnimUtils.fadeOut(getContext(), cvSchedule);
+            AnimUtils.fadeOut(getContext(), svSchedule);
             AnimUtils.fadeIn(getContext(), vgNoSchedule);
         } else {
             AnimUtils.fadeOut(getContext(), vgNoSchedule);
             vgNoSchedule.setVisibility(View.GONE);
-            cvSchedule.setVisibility(View.VISIBLE);
+            svSchedule.setVisibility(View.VISIBLE);
             scheduleAdapter.setLocations(locations);
+            subtitleAdapter.setLocations(locations);
         }
     }
 }
