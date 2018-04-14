8774739755dc8d50751446425096ff493d10e7e3
João Paulo <joaopaulo761@gmail.com>
2018-04-13 21:30:35 -0300

    You kappatino

--- app/src/main/java/com/forcetower/uefs/view/connected/fragments/TheAdventureFragment.java
+++ app/src/main/java/com/forcetower/uefs/view/connected/fragments/TheAdventureFragment.java
@@ -21,7 +21,7 @@ public class TheAdventureFragment extends Fragment implements Injectable {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_the_adventure_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_the_adventure, container, false);
         ButterKnife.bind(this, view);
         return view;
     }
