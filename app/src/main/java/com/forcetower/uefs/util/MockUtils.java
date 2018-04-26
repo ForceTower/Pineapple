92c66ca8e0f56e7bb46c5a85d68e4df9825a3c9a
João Paulo <joaopaulo761@gmail.com>
2018-04-26 16:38:34 -0300

    Code gets a credit for helping with backgrounds

--- app/src/main/java/com/forcetower/uefs/util/MockUtils.java
+++ app/src/main/java/com/forcetower/uefs/util/MockUtils.java
@@ -42,12 +42,14 @@ public class MockUtils {
         Mention lokisley = new Mention("Lokisley \"Lokisssss\" Oliveira", "https://www.facebook.com/Lokisley");
         Mention teixeira = new Mention("Matheus Teixeira", "https://www.facebook.com/teixeirista");
         Mention kuchuki  = new Mention("Marcus \"Kuchuki\" Aldrey", "https://www.facebook.com/marcus.aldrey");
+        Mention rafael   = new Mention("Rafael \"Code\" Azevedo", "https://www.facebook.com/rafaazvd");
 
         Mention bandejao = new Mention("Fonte dos Dados", "http://bit.ly/bandejaouefs");
 
         mentions.add(new CreditsMention("Ícone do Aplicativo", Collections.singletonList(lokisley)));
         mentions.add(new CreditsMention("Nome do Aplicativo", Arrays.asList(teixeira, lokisley, kuchuki)));
         mentions.add(new CreditsMention("Bandejão UEFS", Collections.singletonList(bandejao)));
+        mentions.add(new CreditsMention("Backgrounds", Collections.singletonList(rafael)));
 
         return mentions;
     }
