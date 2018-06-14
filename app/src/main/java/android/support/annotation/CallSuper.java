dd6f6ffe31aaf24c03cd6865cdeba0b8277a6052
João Paulo <joaopaulo761@gmail.com>
2018-06-13 22:43:20 -0300

    Not even compiles

---
+++ app/src/main/java/android/support/annotation/CallSuper.java
@@ -0,0 +1,10 @@
+package com.forcetower.uefs.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+@Retention(CLASS) @Target({METHOD})
+public @interface CallSuper { }
