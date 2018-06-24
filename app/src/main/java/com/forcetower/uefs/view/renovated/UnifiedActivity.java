172146d87b55f2ebd093a016cb3d01013e8090ed
João Paulo <joaopaulo761@gmail.com>
2018-06-24 18:06:48 -0300

    Android 3.2 Beta

---
+++ app/src/main/java/com/forcetower/uefs/view/renovated/UnifiedActivity.java
@@ -0,0 +1,15 @@
+package com.forcetower.uefs.view.renovated;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.forcetower.uefs.R;
+
+public class UnifiedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unified);
+    }
+}
